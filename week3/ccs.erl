1981cbfd7779da0b80bc54555ba7d953aa44a0f9
Jake Rawsthorne <jake@jakerawsthorne.co.uk>
2020-03-05 20:06:23 +0000

    week3 solution

--- week3/ccs.erl
+++ week3/ccs.erl
@@ -1,40 +1,40 @@
 -module(ccs).
--export([do/1, nil/0, p1/0, p2/0, p3/0, p4/0, p5/0, q5/0]).
+
+-export([do/1, nil/0, p1/0, p2/0, p3/0, p4/0, p5/0,
+	 q5/0]).
 
 do([]) -> ok;
-do("a") -> do({input,a});
-do("b") -> do({input,b});
-do("~a") -> do({output,a});
-do("~b") -> do({output,b});
-do("0") -> nil();
 do({output, E}) -> io:format("~~~p~n", [E]);
-do({input, E}) -> io:format("~p~n",[E]);
-do([E|MoreEs]) ->
-    do(E),
-    do(MoreEs).
+do({input, E}) -> io:format("~p~n", [E]);
+do([E | MoreEs]) -> do(E), do(MoreEs).
 
-p1() -> do(["a", "b", "~a", "~b", "0"]).
+p1() ->
+    do({input, a}),
+    do({input, b}),
+    do({output, a}),
+    do({output, b}),
+    nil().
 
-p2() -> do(["a","~b"]), p2().
+p2() -> do({input, a}), do({output, b}), p2().
 
 p3() ->
     case rand:uniform(2) of
-        1 -> do(["a"]);
-        2 -> do(["b"])
+      1 -> do({input, a}), nil();
+      2 -> do({input, b}), nil()
     end.
 
 p4() ->
     case rand:uniform(2) of
-        1 -> do(["a"]), p4();
-        2 -> do(["b"])
+      1 -> do({input, a}), p4();
+      2 -> do({input, b}), nil()
     end.
 
 p5() ->
     case rand:uniform(2) of
-        1 -> do(["a"]), q5();
-        2 -> do(["~b"])
+      1 -> do({input, a}), q5();
+      2 -> do({output, b}), nil()
     end.
 
-q5() -> do(["~b"]), p5().
+q5() -> do({output, b}), p5().
 
 nil() -> ok.
