f33f37714a30336bf071ecc9eb1dd0185d1beb22
Jake Rawsthorne <jake@jakerawsthorne.co.uk>
2020-03-05 21:07:37 +0000

    week4

---
+++ week4/pingpong.erl
@@ -0,0 +1,25 @@
+-module(pingpong).
+
+-export([ping/2, pong/0, start/0]).
+
+ping(0, Pong_PID) ->
+    Pong_PID ! finished, io:format("Ping finished ~n", []);
+ping(N, Pong_PID) ->
+    Pong_PID ! {ping, self()},
+    receive
+      pong -> io:format("Ping received pong ~n", [])
+    end,
+    ping(N - 1, Pong_PID).
+
+pong() ->
+    receive
+      finished -> io:format("Pong finished ~n", []);
+      {ping, Ping_PID} ->
+	  io:format("Pong received ping ~n", []),
+	  Ping_PID ! pong,
+	  pong()
+    end.
+
+start() ->
+    Pong_PID = spawn(pingpong, pong, []),
+    spawn(pingpong, ping, [3, Pong_PID]).
